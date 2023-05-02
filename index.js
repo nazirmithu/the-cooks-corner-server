0ee106ebcbd49ac98a0f0778ddc85b15c2074949
nazirmithu <nazirmithu22@gmail.com>
2023-05-03 00:05:10 +0600

    send all data server site

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const allServices = require('./data/data.json');
+
 app.get('/', (req, res)=>{
 res.send('cooks corner')
 });
+
+app.get("/allData", (req, res)=>{
+    res.send(allServices);
+})
 
 app.listen(port, ()=>{
     console.log(`cooks API is running on port:${port}`)
