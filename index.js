b384c4f799d0c258038f638c7a2d29e5355a2c43
nazirmithu <nazirmithu22@gmail.com>
2023-05-04 19:29:46 +0600

    some update

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ const allServices = require("./data/data.json");
 
 app.use(cors());
 
-app.get('/', (req, res)=>{
-res.send('cooks corner')
+app.get('/', (req, res) => {
+    res.send('cooks corner')
 });
 
-app.get("/allData", (req, res)=>{
+app.get("/allData", (req, res) => {
     console.log(allServices)
     res.send(allServices);
 })
 
-app.listen(port, ()=>{
+app.get("/allData/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id)
+    const Items = allServices?.find(item => parseInt(item.id) === id);
+    res.send(Items);
+})
+
+
+app.listen(port, () => {
     console.log(`cooks API is running on port:${port}`)
 })
